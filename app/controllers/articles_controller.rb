b2224715711534d60cd453b757f5da42fe1bc63d
Danilo Vaz <maskotexd@hotmail.com>
2012-11-26 13:28:56 -0200

    Show articles when published's

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.published
 
     respond_to do |format|
       format.html #index.html.erb
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-# Load the rails application
+#Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # force Rails into production mode when # you don't control web/app server and can't set it the proper way 
